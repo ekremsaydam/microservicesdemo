34829ce52676a87281d1e49957c0823c0f414bdf
DEV\DEVOPS <eksaydam@gmail.com>
2021-06-03 22:47:41 +0300

    initial commit

---
+++ Services/ResAPI/Reservation.API/Controllers/ResController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Reservation.API.Infrastructure;
+using Reservation.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reservation.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResController : ControllerBase
+    {
+        private readonly IReservationService reservationService;
+
+        public ResController(IReservationService ReservationService )
+        {
+            reservationService = ReservationService;
+        }
+
+        [HttpGet("{id}")]
+        public ReservationDTO Get(int id)
+        {
+            return reservationService.GetResByBkgNumber(id);
+        }
+    }
+}
